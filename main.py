d5c73e3f4659a2c72358c00030099c5486c37f2b
Sandun-Induranga <sandutharu40@gmail.com>
2023-11-18 14:12:09 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import main_second
+from main_third import name
+
+print(name)
+
+print('File main.py')
+
+if __name__ == "__main__":
+    print("Main Method of main.py")
---
+++ main_second.py
@@ -0,0 +1,4 @@
+print('File main_second.py')
+
+if __name__ == "__main__":
+    print("Main Method of main_second.py")
